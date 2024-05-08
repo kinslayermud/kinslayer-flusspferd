9c6fb51b86592dd17567c8e18c996ecbd81bfcca
crodnun <crodnun@yahoo.es>
2024-05-08 16:42:33 +0000

    Connect with adapter to use the new JS::Value for legacy jsval

--- src/convert.cpp
+++ src/convert.cpp
@@ -23,6 +23,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+#include "flusspferd/spidermonkey/adapter.hpp"
 #include "flusspferd/convert.hpp"
 #include "flusspferd/array.hpp"
 #include "flusspferd/create.hpp"
