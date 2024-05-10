f5242ac98af62986e47f75fed0445299cc42c81d
crodnun <crodnun@yahoo.es>
2024-05-10 16:20:46 +0000

    Connected with new SpiderMonkey adapter.hpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -23,6 +23,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+
+#include "flusspferd/spidermonkey/adapter.hpp"
+
+
 #include "flusspferd/evaluate.hpp"
 #include "flusspferd/local_root_scope.hpp"
 #include "flusspferd/init.hpp"
