cd95144dd45bd84843f3c2b8d7c026107d1702c4
crodnun <crodnun@yahoo.es>
2024-05-19 11:13:09 +0000

    Migrate old macros: INT_TO_JSVAL and INT_FITS_IN_JSVAL, reference here: https://udn.realityripple.com/docs/Mozilla/Projects/SpiderMonkey/JSAPI_reference/INT_TO_JSVAL

--- src/binary.cpp
+++ src/binary.cpp
@@ -23,6 +23,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+#include "flusspferd/spidermonkey/adapter.hpp"
 #include "flusspferd.hpp"
 #include "flusspferd/binary.hpp"
 #include "flusspferd/evaluate.hpp"
