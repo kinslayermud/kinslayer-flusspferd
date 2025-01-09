6cddf1ed8833c8f4e4dc681ef4877d818aa3d480
crodnun <crodnun@yahoo.es>
2025-01-09 12:29:01 +0000

    Rollback previous change, did not work defining here the missing mozilla functions

--- include/flusspferd/spidermonkey/adapter.hpp
+++ include/flusspferd/spidermonkey/adapter.hpp
@@ -40,9 +40,4 @@ typedef unsigned jsuint;
 typedef unsigned uintN;
 typedef double jsdouble;
 
-// Redefine our own functions to avoid undefined references for those functions no longer present in mozilla's code
-#define moz_arena_malloc(arena, bytes) js_arena_calloc(js::MallocArena, bytes)
-#define moz_arena_calloc(arena, bytes) js_arena_calloc(js::MallocArena, bytes)
-#define moz_arena_realloc(arena, p, bytes) js_arena_realloc(js::MallocArena, p, bytes)
-
 #endif // FLUSSPFERD_SPIDERMONKEY_ADAPTER_HPP
